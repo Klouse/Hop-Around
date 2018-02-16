ea2cf8bac5044939cd27050d696a0031e410c3e7
Nathan Irwin <nki818@mac.com>
2018-02-15 20:05:04 -0800

    start work on starting scene

---
+++ Assets/Hoppy/Scripts/StartScreenUIController.cs
@@ -0,0 +1,279 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+
+// Written By Abdalla Tawfik & Fehr Hassan
+
+
+public class StartScreenUIController : MonoBehaviour {
+
+	#region Variables Declaration
+
+	// A reference to the Player Game Object.
+	public GameObject player;
+
+	// Game Score (steps).
+	[HideInInspector]
+	public int score;
+
+	// A reference to Number of Gems UI Text.
+	public Text numberOfGemsText;
+
+	// Starting Menu - Shown before the game is started.
+	public GameObject startMenu;
+	// Game Menu - Shown during the game is played.
+	public GameObject gameMenu;
+	// Game Over Menu - Shown after the game is over.
+	public GameObject gameOverMenu;
+
+	// Current Score UI Texts (On Game menu and Game Over menu).
+	public Text[] scoreTexts;
+	// Best Score UI Texts (On Starting menu and Game Over menu).
+	public Text[] bestScoreTexts;
+	// Game Name UI Text
+	public Text gameNameText;
+
+	// A reference to all game over panels (Rewarded Video, Free Gifts, New Model, Rate Us and Promotion).
+	public RectTransform[] gameOverPanels;
+	// A reference to the No Enough Gems Group of Elemnts (On New Model Panel).
+	public GameObject noEnoughGems;
+	// A reference to the Enough Gems Group of Elemnts (On New Model Panel).
+	public GameObject EnoughGems;
+	// A reference to Number of Remaining Gems UI Text.
+	public Text numberOfRemainingGemsText;
+
+	public Button settingsButton;
+	public Sprite settingsUnPressed;
+	public Sprite settingsPressed;
+	public Sprite settingsDarkUnPressed;
+	public Sprite settingsDarkPressed;
+
+	public Button submitButton;
+	public Sprite submitUnPressed;
+	public Sprite submitPressed;
+	public Sprite submitDarkUnPressed;
+	public Sprite submitDarkPressed;
+
+	public Camera camera;  // used for background colors
+	public Color [] colors;
+
+	#endregion
+
+
+
+	#region Unity Callbacks
+
+	void Awake ()
+	{
+		// Activate the Start menu Only and deactivate the others.
+		startMenu.SetActive (true);
+		gameMenu.SetActive (false);
+		gameOverMenu.SetActive (false);
+
+
+		// Update Best Score Texts.
+		int bestScore = PlayerPrefs.GetInt("Best Score");
+		updateBestScoreUITexts (bestScore);
+
+		// Update Number of Gems displayed.
+		updateNumberOfGemsUITexts ();
+
+		// Update colors for dark Model
+		updateDarkMode();
+	}
+
+	#endregion
+
+
+
+	#region Game States Methods
+
+	public void onGameStarted ()
+	{
+		// Activate the Game menu Only and deactivate the others.
+		startMenu.SetActive (false);
+		gameMenu.SetActive (true);
+		gameOverMenu.SetActive (false);
+
+		// Increment the number of Games Played.
+		int gamesPlayed = PlayerPrefs.GetInt ("GamesPlayed");
+		gamesPlayed++;
+		PlayerPrefs.SetInt ("GamesPlayed", gamesPlayed);
+	}
+
+	public void onGameOver ()
+	{
+		// Update Best Score Texts according to the new Score.
+		int bestScore = PlayerPrefs.GetInt("Best Score");
+		if (score > bestScore)
+		{
+			PlayerPrefs.SetInt ("Best Score", score);
+			updateBestScoreUITexts (score);
+		}
+		else
+		{
+			updateBestScoreUITexts (bestScore);
+		}
+
+
+		// Activate the Game Over menu Only and deactivate the others.
+		startMenu.SetActive (false);
+		gameMenu.SetActive (false);
+		gameOverMenu.SetActive (true);
+	}
+
+	#endregion
+
+
+
+	#region Updating UI Elements Data
+
+	public void updateScoreUITexts ()
+	{
+		// Update all Score UI Texts with the current score.
+		for (int i = 0; i < scoreTexts.Length; i++)
+		{
+			scoreTexts [i].text = "" + score;
+		}
+	}
+
+	public void updateBestScoreUITexts (int bestScore)
+	{
+		// Update all Best Score UI Texts with the best score.
+		for (int i = 0; i < bestScoreTexts.Length; i++)
+		{
+			bestScoreTexts[i].text = "BEST: " + bestScore;
+		}
+	}
+
+	public void updateNumberOfGemsUITexts ()
+	{
+		// Update Number of Gems displayed.
+		int numberOfGems = PlayerPrefs.GetInt ("NumberOfPickUps");
+		numberOfGemsText.text = "" + numberOfGems;
+	}
+
+	void updateDarkMode()
+	{
+		string darkMode = PlayerPrefs.GetString("dark");
+		if (darkMode == "Off")
+		{
+			changeButtonSprites(settingsButton, settingsUnPressed, settingsPressed);
+			// set background color
+			camera.backgroundColor = colors[0];
+			// set both score and best score texts for menu and game over menu
+			// color should always be opposite of background
+			for (int i = 0; i < scoreTexts.Length; i++)
+			{
+				scoreTexts[i].color = colors[1];
+				bestScoreTexts[i].color = colors[1];
+			}
+			// set gem text color -- always opposite of backgound
+			numberOfGemsText.color = colors[1];
+			// set game name text color -- always opposite of background
+			gameNameText.color = colors[1];
+			// set submit button colors
+			changeButtonSprites(submitButton, submitUnPressed, submitPressed);
+		}
+		else
+		{
+			changeButtonSprites(settingsButton, settingsDarkUnPressed, settingsDarkPressed);
+			// set background color
+			camera.backgroundColor = colors[1];
+			// set both score and best score texts for menu and game over menu
+			// color should always be opposite of background
+			for (int i = 0; i < scoreTexts.Length; i++)
+			{
+				scoreTexts[i].color = colors[0];
+				bestScoreTexts[i].color = colors[0];
+			}
+			// set gem text color -- always opposite of backgound
+			numberOfGemsText.color = colors[0];
+			// set game name text color -- always opposite of background
+			gameNameText.color = colors[0];
+			// set submit button colors
+			changeButtonSprites(submitButton, submitDarkUnPressed, submitDarkPressed);
+		}
+	}
+
+	void changeButtonSprites(Button button, Sprite unpressedImage, Sprite pressedImage)
+	{
+		// Update the sprites of the image component and the pressedSprite of a Button.
+
+		button.GetComponent<Image>().sprite = unpressedImage;
+
+		SpriteState st = new SpriteState();
+		st.pressedSprite = pressedImage;
+		button.spriteState = st;
+  }
+
+
+
+	#endregion
+
+
+
+	#region UI Animation
+
+	IEnumerator animatePanel (RectTransform animatingPanel)
+	{
+		// Calculate the Canvas Size.
+		Vector2 canvasSize = GetComponent<RectTransform> ().sizeDelta;
+
+		// Move the Animating Panel to the left of the screen.
+		animatingPanel.anchoredPosition = new Vector3 (-canvasSize.x, animatingPanel.anchoredPosition.y, 0);
+
+		// Target Position to be reached after Animation is finished.
+		Vector3 targetPosition = new Vector3 (0, animatingPanel.anchoredPosition.y, 0);
+
+		// Animation step in X Axis.
+		float xPosStep = 20;
+
+		while (true)
+		{
+			// Add a step in X position each frame.
+			animatingPanel.anchoredPosition = new Vector3 (animatingPanel.anchoredPosition.x + xPosStep, animatingPanel.anchoredPosition.y, 0);
+
+			if (animatingPanel.anchoredPosition.x >= targetPosition.x)
+			{
+				// Target is Reached or exceeded.
+
+				// Set the position of the Panel with the Targeted Position.
+				animatingPanel.anchoredPosition = targetPosition;
+
+				// Stop the Coroutine.
+				yield break;
+			}
+
+			// Otherwise, continue next frame
+			yield return null;
+		}
+	}
+
+	#endregion
+
+
+
+	#region Buttons onClicked Methods
+
+	public void onSettingsButtonClicked ()
+	{
+		// Load Settings Scene.
+		SceneManager.LoadScene ("Settings");
+	}
+
+	public void onModelShopButtonClicked ()
+	{
+		// Load Models Shop Scene.
+	}
+
+	public void onSubmitButtonClicked ()
+	{
+		SceneManager.LoadScene ("GamePlay");
+	}
+
+	#endregion
+}
