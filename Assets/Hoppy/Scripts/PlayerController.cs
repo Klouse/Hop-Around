f43188f5d8d18269202b2b6670188cc8872884bd
Klouse <Klouse@users.noreply.github.com>
2018-02-28 21:23:45 -0800

    Bugfix - PlayerController.cs
    - Fixed variable name

--- Assets/Hoppy/Scripts/PlayerController.cs
+++ Assets/Hoppy/Scripts/PlayerController.cs
@@ -216,15 +216,15 @@ public class PlayerController : MonoBehaviour {
         {
 		if (PlayerPrefs.GetString("dark") == "Off")
 		{
-			col.gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().material = blackMaterial;
+			go.gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().material = blackMaterial;
 		}
 		else
 		{
-			col.gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().material = whiteMaterial;
+			go.gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().material = whiteMaterial;
 		}
           // Show Boundary Cube.
           go.gameObject.transform.GetChild(0).gameObject.SetActive(true);
-	
+
         }
 
 
